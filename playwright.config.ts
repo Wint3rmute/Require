ec9528ccb78c07797595efe1b0e11b56b549c649
Mateusz Bączek <mateusz.baczek@creotech.pl>
2025-08-17 20:36:38 +0200

    Minor config & CI improvements, add GEMINI.md

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
+const config: import('@playwright/test').PlaywrightTestConfig = {
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -19,8 +19,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -66,4 +64,10 @@ export default defineConfig({
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
-});
+};
+
+if (process.env.CI) {
+  config.workers = 1;
+}
+
+export default defineConfig(config);
